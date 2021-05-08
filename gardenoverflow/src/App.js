23e6c2c3398543ccc8d45a10877ac7dc802a599d
Ariana Winters <arianaw15@gmail.com>
2021-05-08 11:00:41 -0600

    Added new components files

--- gardenoverflow/src/App.js
+++ gardenoverflow/src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header'
 
 function App() {
   return (
     <div>
-      <header>
-       Garden Overflow
-      </header>
+      <Header />
     </div>
   );
 }
