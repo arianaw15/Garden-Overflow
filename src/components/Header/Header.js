41123dc07fcbd26191b1c4e51d3f0b5c04c6ea60
Ariana Winters <arianaw15@gmail.com>
2021-05-08 11:12:31 -0600

    Updated with components folder

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div>
+            <header>
+       Garden Overflow
+      </header>
+        </div>
+    )
+}
+
+export default Header
+
