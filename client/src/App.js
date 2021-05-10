a1bd41f7066f9cfa86585b362e886ed64b6b8997
Landon Ross <landon.w.ross@gmail.com>
2021-05-10 08:19:32 -0600

    fixed file structure and bootstrap compatability

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header/Header'
+import Overgrowth from "./components/Overgrowth/Overgrowth.js"
 
 function App() {
   return (
     <div>
       <Header />
+      <Overgrowth />
     </div>
   );
 }
---
+++ client/src/components/Overgrowth/Overgrowth.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Overgrowth() {
+    return (
+        <div>
+	<section class="p12">
+		<a href="/"  data-content="peak">peak</a>
+	</section>
+        </div>
+    )
+}
+
+export default Overgrowth
