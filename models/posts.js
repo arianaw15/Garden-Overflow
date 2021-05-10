69e88abb907bd72d01c919ddd2e7fe26de38608e
Jpeyton <jpwalk319@gmail.com>
2021-05-10 13:06:29 -0600

    updated posts schema

--- models/posts.js
+++ models/posts.js
@@ -4,10 +4,8 @@ const Schema = mongoose.Schema;
 const postSchema = new Schema({
   userName: { type: String, required: true },
   email: { type: String, required: true },
-  password: { type: String, required: true },
-  zone: { type: Number, required: true },
-  garden: { type: Array },
-  zip: { type: Number, required: true }
+  title: { type: String, required: true },
+  body: { type: String, required: true }
 });
 
 const Posts = mongoose.model("posts", postSchema);
