f9d948e2dfa3d075152f3ba3cdd8a5f173e92028
Landon Ross <landon.w.ross@gmail.com>
2021-05-10 09:13:35 -0600

    added navbar variability

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
+import Pagination from '../Pagination/Pagination.js';
+import './index.css';
 
 function Header() {
     return (
         <div>
             <header>
-       Garden Overflow
-      </header>
+                <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+                    <a className="navbar-brand" href="/"><span className="garden">Garden</span> Overflow</a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <Pagination />
+                </nav>
+            </header>
         </div>
     )
 }
