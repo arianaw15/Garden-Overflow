c8fde71e800e3a4e40deac4761bc3c9a03c5a891
Landon Ross <landon.w.ross@gmail.com>
2021-05-10 09:21:31 -0600

    added bootstrap jquery functionality to HTML

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -9,7 +9,7 @@ function Header() {
                 <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
                     <a className="navbar-brand" href="/"><span className="garden">Garden</span> Overflow</a>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
+                        <span className="navbar-toggler-icon" id="navbarToggler"></span>
                     </button>
                     <Pagination />
                 </nav>
