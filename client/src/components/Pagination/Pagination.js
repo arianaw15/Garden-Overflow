d8e7353c83e6b2250ba9e767f8c9617781748527
Landon Ross <landon.w.ross@gmail.com>
2021-05-11 10:44:46 -0600

    added routers for pages

--- client/src/components/Pagination/Pagination.js
+++ client/src/components/Pagination/Pagination.js
@@ -5,9 +5,9 @@ function Pagination() {
     return (
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div className="navbar-nav">
-            <a className="nav-link active" href="/">Home <span className="sr-only">(current)</span></a>
-            <a className="nav-link" href="/">Features</a>
-            <a className="nav-link" href="/">Pricing</a>
+            <a className="nav-link active" href="/Home">Home <span className="sr-only">(current)</span></a>
+            <a className="nav-link" href="/Login">Login</a>
+            <a className="nav-link" href="/">Create Account</a>
             {/* <a className="nav-link disabled" href="/" tabindex="-1" aria-disabled="true">Disabled</a> */}
         </div>
     </div>
