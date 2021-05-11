7ac64e922cad73506015e9fc9b9db5b35f38cb62
Landon Ross <landon.w.ross@gmail.com>
2021-05-11 10:18:08 -0600

    fixed welcome page css

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import Header from './components/Header/Header.js';
-import LandingPage from './components/LandingPage/LandingPage.js';
+// import LandingPage from './components/LandingPage/LandingPage.js';
+import LoginForm from "./components/LoginForm/LoginForm";
 
 
 function App() {
   return (
     <div>
       <Header />
-      <LandingPage />
+      {/* <LandingPage /> */}
+      <LoginForm />
     </div>
   );
 }
