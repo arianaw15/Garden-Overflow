ed0ea678f29d19200e1065cbbce7c50343208f42
Jpeyton <jpwalk319@gmail.com>
2021-05-13 11:08:05 -0600

    added header and footer to the forum page

--- client/src/components/Forum/ZoneForum.js
+++ client/src/components/Forum/ZoneForum.js
@@ -1,13 +1,19 @@
 import React from "react";
 import './index.css';
 import Postcard from './Postcard'
+import Header from '../Header/Header'
+import Footer from '../Footer/Footer'
 import axios from 'axios'
 
 const ZoneForum = () => {
     let zone = 7;
     return (
+        <div>
+            <Header />
         <div className="container">
             {axios.get(`/api/zoneposts/${zone}`).then(result => result.map(each => <Postcard {...each}/>))}
+        </div>
+            <Footer />
         </div>
     )
 }
