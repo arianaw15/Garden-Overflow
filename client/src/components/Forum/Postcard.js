b85df69eda3f0e9bbf7cdd7e1ae4e2fddfe4ee47
Jpeyton <jpwalk319@gmail.com>
2021-05-13 10:34:16 -0600

    formatted postcard.js

--- client/src/components/Forum/Postcard.js
+++ client/src/components/Forum/Postcard.js
@@ -1,24 +1,22 @@
 import React from "react";
-import './index.css';
+import "./index.css";
 
-const Postcard = ({username, title, body, zone, tag, email}) => {
+const Postcard = ({ username, title, body, zone, tag, email }) => {
   return (
     <div className="row justify-content-md-center">
-    <div className="card col">
+      <div className="card col">
         <div className="card-header">
-            <a href="#">{username}</a> Posted to Zone: {zone}
+          <a href="#">{username}</a> Posted to Zone: {zone}
         </div>
-      <div className="card-body">
-        <h4 className="card-title">{title}</h4>
-        <h6 className="card-subtitle mb-2 text-muted">{tag}</h6>
-        <p className="card-text">
-          {body}
-        </p>
-        <a href="#" className="card-link">
-          View replies
-        </a>
+        <div className="card-body">
+          <h4 className="card-title">{title}</h4>
+          <h6 className="card-subtitle mb-2 text-muted">{tag}</h6>
+          <p className="card-text">{body}</p>
+          <a href="#" className="card-link">
+            View replies
+          </a>
+        </div>
       </div>
-    </div>
     </div>
   );
 };
