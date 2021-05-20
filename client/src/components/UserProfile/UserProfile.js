4053225084779115a20a1b6b0b2b54b253de14b4
Brandon Ashby <bash7325@gmail.com>
2021-05-19 20:02:23 -0600

    able to retrieve user attributes on profile, fixed sign out

--- client/src/components/UserProfile/UserProfile.js
+++ client/src/components/UserProfile/UserProfile.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './index.css';
+import { Auth } from 'aws-amplify';
 
 function UserProfile() {
+    let [cognitoUser, setCognitoUser] = useState({})
+    useEffect(() => {
+      
+    Auth.currentAuthenticatedUser({
+        bypassCache: false  // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
+    }).then(user => setCognitoUser(user))
+    .catch(err => console.log(err));
+}, []);
+    console.log(cognitoUser)
+
     return (
         <div>
 <div className="row py-5 px-4">
@@ -11,7 +22,7 @@ function UserProfile() {
                 <div className="media align-items-end profile-head">
                     <div className="profile mr-3"><img src="https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80" alt="..." width="130" className="rounded mb-2 img-thumbnail" /><a href="/" id="editButton" className="btn btn-outline-dark btn-sm btn-block">Edit Profile</a></div>
                     <div className="media-body mb-5 text-white">
-                        <h4 className="mt-0 mb-0">Test User</h4>
+                        <h4 className="mt-0 mb-0">{[]}</h4>
                         <p className="small mb-4"> <i className="fas fa-map-marker-alt mr-2"></i>Salt Lake City</p>
                     </div>
                 </div>
