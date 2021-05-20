9ddeab5a8679df2ae53a73c6c7bf3418d7c35654
Jpeyton <jpwalk319@gmail.com>
2021-05-20 10:15:24 -0600

    finished formatting replyboard

--- client/src/components/Forum/Commentform.js
+++ client/src/components/Forum/Commentform.js
@@ -2,20 +2,20 @@ import React from "react";
 
 const Commentform = ({ postid }) => {
   return (
-      <div className="formtainer">
+      <div id="commentform" className="formtainer row justify-content-md-center">
       <form>
-        <div className="mb-3">
+        
           <label htmlFor="Textarea1" className="form-label">
             Your Comment
           </label>
           <textarea
             className="form-control"
             id="Textarea1"
-            rows="3"
+            rows="1"
             name="body"
           ></textarea>
         <button type="submit" class="btn btn-primary">Submit</button>
-        </div>
+        
       </form>
       </div>
     
