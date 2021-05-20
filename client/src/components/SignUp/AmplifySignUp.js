9ce7c396b390a24046137de717cc4de18272576f
Brandon Ashby <bash7325@gmail.com>
2021-05-19 21:28:33 -0600

    sign up page has correct user attributes

--- client/src/components/SignUp/AmplifySignUp.js
+++ client/src/components/SignUp/AmplifySignUp.js
@@ -1,75 +1,79 @@
 /* eslint-disable default-case */
-import React, {useEffect} from 'react';
-import './index.css'
-import Amplify, {Hub, Auth} from 'aws-amplify';
-import { AmplifyAuthenticator, AmplifySignUp} from '@aws-amplify/ui-react';
-import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
-import awsconfig from '../../aws-exports';
-import AmplifyGoogle from './AmplifyGoogle';
-import userState from '../../utils/UserState';
-import { useRecoilState } from 'recoil';
-import { Redirect } from 'react-router';
+import React, { useEffect } from "react";
+import "./index.css";
+import Amplify, { Hub, Auth } from "aws-amplify";
+import { AmplifySignUp } from "@aws-amplify/ui-react";
+import awsconfig from "../../aws-exports";
+import userState from "../../utils/UserState";
+import { useRecoilState } from "recoil";
+import { Redirect } from "react-router";
 
 Amplify.configure(awsconfig);
 
 function AmpSignUp() {
-    // const [authState, setAuthState] = React.useState();
-    // const [user, setUser] = React.useState();
-
-    // React.useEffect(() => {
-    //     return onAuthUIStateChange((nextAuthState, authData) => {
-    //         setAuthState(nextAuthState);
-    //         setUser(authData)
-    //     });
-    // }, []);
-
-    const [user, setUser] = useRecoilState(userState);
+  const [user, setUser] = useRecoilState(userState);
 
   useEffect(() => {
-    Hub.listen('auth', ({ payload: { event, data } }) => {
+    Hub.listen("auth", ({ payload: { event, data } }) => {
       switch (event) {
-        case 'signIn':
-        case 'cognitoHostedUI':
-          getUser().then(userData => setUser(userData));
+        case "signIn":
+        case "cognitoHostedUI":
+          getUser().then((userData) => setUser(userData));
           break;
-        case 'signOut':
+        case "signOut":
           setUser(null);
           break;
-        case 'signIn_failure':
-        case 'cognitoHostedUI_failure':
-          console.log('Sign in failure', data);
+        case "signIn_failure":
+        case "cognitoHostedUI_failure":
+          console.log("Sign in failure", data);
           break;
       }
     });
 
-    getUser().then(userData => setUser(userData));
+    getUser().then((userData) => setUser(userData));
   }, []);
 
   function getUser() {
     return Auth.currentAuthenticatedUser()
-      .then(userData => userData)
-      .catch(() => console.log('Not signed in'));
+      .then((userData) => userData)
+      .catch(() => console.log("Not signed in"));
   }
   console.log(user);
 
   //Return
 
-  return user ? <Redirect to = {'/userprofile'} /> : (
-      
-        <div>
-            <AmplifySignUp className="amplify-signup"
-          slot="sign-up"
-          formFields={[
-            { type: "username" },
-            { type: "password" },
-            { type: "email" },
-            {type:"locale"}
-          ]}
-        />
-        
-        </div>
+  return user ? (
+    <Redirect to={"/userprofile"} />
+  ) : (
+    <div>
+      <AmplifySignUp
+        className="amplify-signup"
+        slot="sign-up"
+        formFields={[
+          {
+            type: "username",
+            label: "Email:",
+            placeholder: "Enter your email address",
+          },
+          {
+            type: "email",
+            label: "Verify Email:",
+            placeholder: "Verify your email address",
+          },
+          { 
+            type: "password" 
+          },
+          { type: "locale", 
+          label: "Zip Code:",
+          placeholder: "Zip code" 
+          },
+          { type: "nickname",
+            label: "Create a unique user name:"
+          },
+        ]}
+      />
+    </div>
   );
-        }
-
+}
 
 export default AmpSignUp;
