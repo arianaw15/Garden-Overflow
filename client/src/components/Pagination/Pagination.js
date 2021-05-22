dcb38d4da624a52fbb7cd3bce61d2014b3600629
Brandon Ashby <bash7325@gmail.com>
2021-05-22 11:54:43 -0600

    repeat button weirdness

--- client/src/components/Pagination/Pagination.js
+++ client/src/components/Pagination/Pagination.js
@@ -11,9 +11,8 @@ function Pagination() {
             <a className="nav-link" href="/SignUp">Create Account</a>
             <a className="nav-link" href="/Zoneforum">MyZone</a>
             <a className="nav-link" href="/userProfile">My Profile</a>
-            <a href="/Home"><SignOutBtn /></a>
             <a className="nav-link" href="/PlantPage">Plant Library</a>
-            <SignOutBtn />
+            <a href="/Home"><SignOutBtn /></a>
             {/* <a className="nav-link disabled" href="/" tabindex="-1" aria-disabled="true">Disabled</a> */}
         </div>
     </div>
