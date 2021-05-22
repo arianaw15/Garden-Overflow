038e61c4a8b6fa43904fafd192385efccd62453b
Ariana Winters <arianaw15@gmail.com>
2021-05-22 13:12:01 -0600

    updated plant table

--- client/src/components/PlantPage/PlantPage.js
+++ client/src/components/PlantPage/PlantPage.js
@@ -1,11 +1,26 @@
-import React from 'react';
-import PlantColumn from './PlantColumn'
+import React, {useEffect, useState} from 'react';
+import './index.css'
+// import PlantColumn from './PlantColumn';
+import PlantCard from './PlantCard'
+import { Row, Col, Container } from 'react-bootstrap';
 
-function PlantPage() {
+function PlantPage({zone}) {
+    const [plantTable, setPlantTable] = useState([]);
+  useEffect(() => {
+    fetch(`/api/plants/${zone || "4"}`)
+      .then((res) => {
+        
+        return res.json()})
+      .then((data) => {
+        console.log(data);
+        setPlantTable(data)});
+  }, []);
     return (
-        <div>
-            <PlantColumn />
-        </div>
+        <Container>
+            <Row className="plantcard" md={3}>
+        {plantTable.length ? (plantTable.map(each => <Col className="plant" key={each.id} ><PlantCard {...each} /></Col>)) : (<h3>No Posts in this zone currently!</h3>)}
+      </Row>
+      </Container>
     )
 }
 
