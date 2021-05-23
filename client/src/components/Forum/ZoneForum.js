75d1c2ddbf23a961fbc9b6557023a996f6287da4
Jpeyton <jpwalk319@gmail.com>
2021-05-23 13:22:26 -0600

    added first part of user auth to zoneforum

--- client/src/components/Forum/ZoneForum.js
+++ client/src/components/Forum/ZoneForum.js
@@ -2,7 +2,12 @@ import { React, useState, useEffect } from "react";
 import "./index.css";
 import Postcard from "./Postcard";
 import Postform from "./Postform";
+import { Auth } from 'aws-amplify';
 
+Auth.currentAuthenticatedUser({
+    bypassCache: false  // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
+}).then(user => console.log(user))
+.catch(err => console.log(err));
 
 const ZoneForum = () => {
   const [posts, setPosts] = useState([{zone : "7"}]);
