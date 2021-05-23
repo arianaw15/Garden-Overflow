8019e9eb8214d3b5f94e49c123359ceab5fad7b9
Landon Ross <landon.w.ross@gmail.com>
2021-05-23 15:18:07 -0600

    fixed plant cards

--- client/src/components/PlantPage/PlantCard.js
+++ client/src/components/PlantPage/PlantCard.js
@@ -5,9 +5,9 @@ import { Card, Button } from 'react-bootstrap';
 
 function PlantCard(props) {
     return (
-        <Card className= "plant-card" style={{ width: 'auto' }}>
-        <Card.Img style={{height: 'auto'}} variant="top" src={props.image_url} />
-        <Button className="plant-button" type="submit">Add {props.name} to Your Garden</Button>
+        <Card className= "plant-card h-100" style={{ width: 'auto' }}>
+        <Card.Img className="h-75"style={{height: 'auto'}} variant="top" src={props.image_url} />
+        <Button className="plant-button h-25" type="submit">Add {props.name} to Your Garden</Button>
         
       </Card>
     );
