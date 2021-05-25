9228468c29f6c9afd4008a01a6f05e3325f186fb
Landon Ross <landon.w.ross@gmail.com>
2021-05-25 14:51:09 -0600

    minor style changes

--- client/src/components/UserProfile/UserProfile.js
+++ client/src/components/UserProfile/UserProfile.js
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-import React, { useState, useEffect } from 'react';
-import './index.css';
-import { Auth } from 'aws-amplify';
-import LoggedHeader from '../LoggedHeader/LoggedHeader.js';
-import ScrollToTop from '../ScrollToTop/scrollToTop';
-import comingSoon from './comingSoon.png';
-
-=======
 import React, { useState, useEffect } from "react";
 import "./index.css";
 import { Auth } from "aws-amplify";
@@ -14,7 +5,7 @@ import LoggedHeader from "../LoggedHeader/LoggedHeader.js";
 import ScrollToTop from "../ScrollToTop/scrollToTop";
 import GardenCard from "./GarderCard";
 import { Row, Col, Container } from "react-bootstrap";
->>>>>>> 19b021bf8e0678bc1ab2cd1228158756e6c53437
+import comingSoon from './comingSoon.png';
 
 function UserProfile() {
   let [user, setUser] = useState(false);
@@ -31,36 +22,6 @@ function UserProfile() {
   }, []);
   
 
-<<<<<<< HEAD
-
-    let [cognitoUser, setCognitoUser] = useState({})
-    useEffect(() => {
-
-        Auth.currentAuthenticatedUser({
-            bypassCache: true  // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
-        }).then(user => {
-            console.log(user);
-            setCognitoUser(user)})
-            .catch(err => console.log(err));
-    }, []);
-    console.log(cognitoUser)
-    return (
-        <div>
-        <LoggedHeader />
-            <div className="row py-5 px-4">
-                <div className="col-md-12 mx-auto">
-                    <div className="bg-white shadow rounded overflow-hidden">
-                        <div className="px-4 pt-0 pb-4 cover">
-                            <div className="media align-items-end profile-head">
-                                <div className="profile mr-3"><img src={comingSoon} alt="..." width="130" className="rounded mb-2 img-thumbnail" /><a href="/UserAttributes" id="editButton" className="btn btn-outline-light btn-sm btn-block">Edit Profile</a></div>
-                                <div className="media-body mb-5 text-white">
-                                    <h4 className="mt-0 mb-0" id="userName">{!cognitoUser.attributes ? "Stand By..." : cognitoUser.attributes.nickname}</h4>
-                                    {/* <p className="small mb-4" id="userName"> <i className="fas fa-map-marker-alt mr-2"></i>Marc Zooleo</p> */}
-                                </div>
-                            </div>
-                        </div>
-                        {/* <div className="bg-light p-4 d-flex justify-content-end text-center">
-=======
   useEffect(() => {
     fetch(
       `/api/getuser/${
@@ -88,7 +49,7 @@ function UserProfile() {
               <div className="media align-items-end profile-head">
                 <div className="profile mr-3">
                   <img
-                    src="https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80"
+                    src={comingSoon}
                     alt="..."
                     width="130"
                     className="rounded mb-2 img-thumbnail"
@@ -112,7 +73,6 @@ function UserProfile() {
               </div>
             </div>
             {/* <div className="bg-light p-4 d-flex justify-content-end text-center">
->>>>>>> 19b021bf8e0678bc1ab2cd1228158756e6c53437
                 <ul className="list-inline mb-0">
                     <li className="list-inline-item">
                         <h5 className="font-weight-bold mb-0 d-block">215</h5><small className="text-muted"> <i className="fas fa-image mr-1"></i>Photos</small>
