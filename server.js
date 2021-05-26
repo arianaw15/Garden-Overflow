dd324f5abefe3caeae406cdf030cc9da86bdacb1
Ariana Winters <arianaw15@gmail.com>
2021-05-25 19:17:41 -0600

    updating server to work on heroku

--- server.js
+++ server.js
@@ -9,9 +9,18 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
+
+if(process.env.NODE_ENV === 'production'){
+  //set static folder
+  app.use(express.static('client/build'));
 }
+
+app.get('*',(req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 // Add routes, both API and view
 app.use('/api', routes);
